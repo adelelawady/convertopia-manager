95d87bc9b690609c7b483f355c30bc4c9b6186fa
Adel Elawady <adel50ali50@gmail.com>
2024-11-23 22:01:35 +0200

    Updates

--- serviceworker.js
+++ serviceworker.js
@@ -3,24 +3,24 @@ self.addEventListener('fetch', (event) => {
     const url = new URL(event.request.url);
     
     // Handle FFmpeg worker requests
-     if (url.href.includes('@ffmpeg/') || url.href.includes('ffmpeg-core')  || url.href.includes('assets') ) {
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          const newHeaders = new Headers(response.headers);
-          newHeaders.set('Cross-Origin-Embedder-Policy', 'require-corp');
-          newHeaders.set('Cross-Origin-Resource-Policy', 'cross-origin');
-          newHeaders.set('Access-Control-Allow-Origin', '*');
-          
-          return new Response(response.body, {
-            status: response.status,
-            statusText: response.statusText,
-            headers: newHeaders,
-          });
-        })
-    );
-    return;
-  }
+    if (url.href.includes('@ffmpeg/') || url.href.includes('ffmpeg-core')) {
+        event.respondWith(
+          fetch(event.request)
+            .then(response => {
+              const newHeaders = new Headers(response.headers);
+              newHeaders.set('Cross-Origin-Embedder-Policy', 'require-corp');
+              newHeaders.set('Cross-Origin-Resource-Policy', 'cross-origin');
+              newHeaders.set('Access-Control-Allow-Origin', '*');
+              
+              return new Response(response.body, {
+                status: response.status,
+                statusText: response.statusText,
+                headers: newHeaders,
+              });
+            })
+        );
+        return;
+      }
   
     if (event.request.mode === 'navigate') {
       event.respondWith(
